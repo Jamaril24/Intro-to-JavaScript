d724e31a7812329ad6ae5acaac8ac1537e8d3fac
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2021-10-22 10:03:17 -0700

    tried something, didnt work

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello')
+
+document. write ("Attempting to work JavaScript!")
+
+document. write ("lowkey hard")
+
+var name = "Jamari"
+
+var numb = 7
+
+var numb2= 3
+console.log(Name);
+console.log(numb + numb2);
+
+function addition(numb1, numb2){
+    console.log(numb1+numb2);
+}
+addition(5,22):
+addition (7,3):
