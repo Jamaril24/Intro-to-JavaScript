ddd2ca8ec1e2fe8d5c3820c40de3bdc98f82144c
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2021-10-27 09:14:16 -0700

    done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log ("sup")
+console.log ("Helloo")
 
-var name = "J"
+var name = "Jamari"
 
 var numb = 7
 
@@ -12,11 +12,30 @@ console.log(numb + numb2);
 function addition(numb, numb2){
     console.log(numb + numb2);
 }
-addition(13, 5);
-addition(50, 2);
 
 function introduction(name, age) {
     console.log(' hello my name is ' + name + ' and i am ' + age + ' years old ');
 }
 
 introduction('Jamari', "15");
+
+
+
+console.log ('I think my sctipt is working');
+
+document.write(<h1>The introduction to my JavaScript</h1>)
+document.write(<h3>Code201 project on JavaScript</h3>)
+
+alert ('This is a random alert to see if this works')
+
+alert ('Another alert for no reason ')
+
+alert ('This alert is too annoy you')
+
+alert ('Kk done now')
+
+alert ('haha one more, now im done have a nice day!')
+
+prompt ('This is like an alert but you can type something!so hows your day')
+
+prompt (' Okay im offically done with these. Have a good day')
